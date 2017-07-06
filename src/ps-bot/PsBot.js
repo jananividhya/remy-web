4bcbed4d6a10229f8594fac31585939c39410919
Shabeer Mothi <shabeer@purpleslate.in>
2017-07-06 08:30:22 -0400

    Connect to bot with an activity

--- src/ps-bot/PsBot.js
+++ src/ps-bot/PsBot.js
@@ -4,8 +4,11 @@ import React, { Component } from 'react';
 // Material UI imports
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {List, ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
 import Avatar from 'material-ui/Avatar';
+import TextField from 'material-ui/TextField';
+import {orange500, blue500} from 'material-ui/styles/colors';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentSend from 'material-ui/svg-icons/content/send';
 
 // Common imports
 import 'whatwg-fetch';
@@ -20,24 +23,56 @@ const psBotStyle = {
     marginBottom: 10
 };
 
+const psBotConversationInputStyle = {
+    errorStyle: {
+        color: orange500,
+    },
+    underlineStyle: {
+        borderColor: orange500,
+    },
+    floatingLabelStyle: {
+        color: orange500,
+    },
+    floatingLabelFocusStyle: {
+        color: blue500,
+    }
+};
+
 /**
  * @class PsBot
  * @extends Component
  * @description pS Bot Component which renders the bot as a web client
  */
 class PsBot extends Component {
+    /**
+     * @todo move directLineBaseUrl to config file
+     * @type {string}
+     */
+    directLineBaseUrl = "https://directline.botframework.com/v3/directline";
+    headers;
+
     constructor(props) {
         super(props);
 
         // Needed for onTouchTap
         injectTapEventPlugin();
 
-        const directLineBaseUrl = "https://directline.botframework.com/v3/directline";
         this.state = {
-            conversationId: ''
+            conversationId: '',
+            conversationText: '',
+            conversations: [],
+            conversationInputText: 'Begin your conversation here..'
         };
 
-        this.initConversation(directLineBaseUrl);
+        /**
+         * @todo move headers to config file
+         * @type {*}
+         */
+        this.headers = new Headers();
+        this.headers.append('Authorization', 'Bearer 052B98YOnWs.cwA.VvI.cQBah7daXBPxhRRJwxMwGVc06fh0-G4rB3hwLFtS7S4');
+        this.headers.append('Content-Type', 'application/json');
+
+        this.initConversation(this.directLineBaseUrl);
     }
 
     /**
@@ -52,11 +87,8 @@ class PsBot extends Component {
             "eTag": ""
         };
 
-        let headers = new Headers();
-        headers.append('Authorization', 'Bearer 052B98YOnWs.cwA.VvI.cQBah7daXBPxhRRJwxMwGVc06fh0-G4rB3hwLFtS7S4');
-
         let request = new Request(directLineBaseUrl + '/conversations',
-            {method: 'POST', headers: headers, body: JSON.stringify(conversationObj)});
+            {method: 'POST', headers: this.headers, body: JSON.stringify(conversationObj)});
 
         fetch(request)
             .then((response) => {
@@ -74,38 +106,101 @@ class PsBot extends Component {
 
 
     /**
-     * @method getBotConversations()
-     * @methodOf PsBot#getBotConversations
-     * @description Gets the bot responses for a conversation from user
+     * @method sendConversationToBot()
+     * @methodOf PsBot#sendConversationToBot
+     * @description Sends the user conversation to pS Bot
      */
-    getBotConversations() {
-        fetch('./PsBot.json')
-            .then(function(response) {
+    sendConversationToBot = () => {
+        let conversation = {
+            "type": "message",
+            "text": this.state.conversationText,
+            "from": {
+                "id": "default-user",
+                "name": "User"
+            },
+            "locale": "en-US",
+            "textFormat": "plain",
+            "timestamp": new Date(),
+            "channelData": {
+                "clientActivityId": "31a9cca1-0245-47f1-9889-5aebd49ccbbf"
+            },
+            "id": "1253e4ba-90d7-435b-95bf-8f2ad30441c9"
+        };
+
+        let request = new Request(this.directLineBaseUrl + '/conversations/' + this.state.conversationId + '/activities',
+            {method: 'POST', headers: this.headers, body: JSON.stringify(conversation)});
+
+        let conversations = this.state.conversations;
+        conversations.push(conversation);
+
+        fetch(request)
+            .then((response) => {
                 return response.json();
-            }).then(function(json) {
-            console.log('Parsed JSON ', json);
-        }).catch(function(ex) {
-            console.log('Parsing failed ', ex);
+            }).then((json) => {
+            this.setState({
+                conversationId: this.state.conversationId,
+                conversationText: '',
+                conversations: conversations,
+                conversationInputText: this.state.conversationInputText
+            });
+        }).catch((ex) => {
+            console.log('Parsing failed while sending conversation to bot ', ex);
         });
-    }
+    };
+
+    /**
+     * @method setConversation
+     * @methodOf PsBot#setConversation
+     * @description Sets the conversation state
+     * @param event
+     */
+    setConversation = (event) => {
+        this.setState({
+            conversationId: this.state.conversationId,
+            conversationText: event.target.value,
+            conversations: this.state.conversations,
+            conversationInputText: 'Conversation here..'
+        });
+    };
 
     render() {
         return (
             <div style={psBotStyle}>
                 Bot initialized for conversation id {this.state.conversationId}
                 <List>
-                    <ListItem
-                        leftAvatar={<Avatar src="images/ok-128.jpg" />}
-                        primaryText="Message"
-                        secondaryText={
-                            <p>
-                                Test User Message
-                            </p>
-                        }
-                        secondaryTextLines={2}
-                    />
-                    <Divider inset={true} />
+                    /**
+                     * @todo add css
+                     */
+                    {this.state.conversations.map((conversation, id) => {
+                        return <ListItem key={id}
+                            leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                            primaryText={conversation.from.name}
+                            secondaryText={
+                                <p>
+                                    {conversation.text}
+                                </p>
+                            }
+                            secondaryTextLines={1}
+                        />
+                    })}
                 </List>
+                <div className="Ps-Bot-Conversation-Input-Container">
+                    /**
+                     * @todo add css
+                     */
+                    <form>
+                        <TextField
+                            floatingLabelText={this.state.conversationInputText}
+                            floatingLabelStyle={psBotConversationInputStyle.floatingLabelStyle}
+                            floatingLabelFocusStyle={psBotConversationInputStyle.floatingLabelFocusStyle}
+                            value={this.state.conversationText}
+                            onChange={this.setConversation}
+                        />
+                        <FloatingActionButton mini={true} onClick={this.sendConversationToBot}>
+                            <ContentSend />
+                        </FloatingActionButton>
+                    </form>
+                </div>
             </div>
         );
     }

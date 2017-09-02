70f18e081e61e30546ec083a3b7d23969d9367b5
Shabeer Mothi <shabeer@Shabeers-MacBook-Pro.local>
2017-09-02 13:21:06 -0400

    Moved style

---
+++ src/components/ps-bot/styles/PsBotCardStyle.js
@@ -0,0 +1,52 @@
+const psBotCardStyle = {
+    buttonResponse: {
+        boxShadow: '0px 0px',
+        color: '#FFFFFF',
+        fontFamily: 'Lato, sans-serif !important',
+        minWidth: '120px',
+        borderRadius: '60px',
+        height: '10px',
+        background: 'rgba(150, 101, 171, 0.87)',
+    },
+    nextLine: {
+        wordWrap: 'break-word',
+        clear: 'both',
+        position: 'relative',
+        overflowY: 'scroll',
+    },
+    card: {
+        maxWidth: 345,
+    },
+    leftAlignedText: {
+        float: 'left !important',
+        textAlign: 'left !important',
+    },
+    psTextColor: {
+        fontFamily: 'Lato, sans-serif',
+        color: '#9B9B9B',
+    },
+    buttonTop: {
+        bottom: '4px',
+    },
+    paperBotConversation: {
+        background: '#FFFFFF',
+        color: '#9B9B9B',
+        boxShadow: '0px 0px',
+        border: '1px solid #D2D1D2',
+        borderRadius: '15px',
+        fontSize: '14px',
+        float: 'right',
+        textAlign: 'right',
+        cursor: 'pointer',
+        paddingRight: '10px',
+        paddingLeft: '10px',
+        position: 'relative',
+        maxWidth: '450px',
+    },
+    conversationText: {
+        marginTop: '-8px',
+        marginBottom: '-8px',
+    },
+};
+
+exports.psBotCardStyle = psBotCardStyle;

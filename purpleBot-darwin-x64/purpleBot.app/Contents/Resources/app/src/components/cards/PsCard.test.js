b3cc10d64a75296c59c98dbd437f7a5070c4d866
Shabeer Mothi <shabeer@purpleslate.in>
2017-12-01 00:37:08 -0500

    Remove desktop build

--- purpleBot-darwin-x64/purpleBot.app/Contents/Resources/app/src/components/cards/PsCard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PsBot from './PsBot';
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PsBot />, div);
-});

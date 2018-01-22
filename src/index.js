6e7ed6e74710868fada905c1ed1ea70370b010fd
Shabeer Mothi <shabeer@purpleslate.in>
2018-01-22 13:15:22 -0500

    Fix scroll

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const paperStyle = {
     marginTop: (window.parent.remy) ? ((cuiTheme.hideHeader) ? -50 : 0) : 0,
     marginLeft: 0,
     display: 'inline-block',
-    overflow: 'hidden',
+    overflow: 'scroll',
     boxShadow: '0px 0px',
     border: '0px solid #D2D1D2',
     background: remyDress.background || '#FFFFFF',

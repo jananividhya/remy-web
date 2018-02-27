3350bcf61b1b5e4135d66eb8359dbbe3d2d80f92
Shabeer <shabeer@purpleslate.in>
2018-02-27 17:00:07 -0500

    fix(Sign In Bubble): Fix sign-in bubble dimensions

--- src/components/ps-bot/PsSlashCommand.js
+++ src/components/ps-bot/PsSlashCommand.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default (props) => (
-    <span style={{
-        fontFamily: 'monospace',
-        color: props.color || '#DA3D4F',
-        backgroundColor: props.background || '#F7F7F9'
-    }}>
-        {(props.commandText.charAt(0) === '/') ? 
-            (props.commandText.substring(1, props.commandText.length)) : props.commandText}
-    </span>
-);
+import PsMarkdown from '../markdown/PsMarkdown';
+
+export default (props) => {
+    const conversationText = (props.commandText.charAt(0) === '/') ?
+        (props.commandText.substring(1, props.commandText.length)) : props.commandText;
+
+    return (
+        <PsMarkdown
+            text = {conversationText} />
+    );
+};

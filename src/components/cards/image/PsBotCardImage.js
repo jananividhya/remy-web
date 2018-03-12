e4fef82cdc389a629c771d257cb289e7e45115e4
Shabeer <shabeer@purpleslate.in>
2018-03-11 22:01:44 -0400

    feat(Image Card): Add ability to zoom images in an image card

--- src/components/cards/image/PsBotCardImage.js
+++ src/components/cards/image/PsBotCardImage.js
@@ -4,12 +4,20 @@ import React, {Component} from 'react';
 // Material UI imports
 import {CardMedia} from 'material-ui/Card';
 
+import { Image, Modal } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
 /**
  * @class PsBotCardImage
  * @extends Component
  * @description pS Bot Image Card Response
  */
 class PsBotCardImage extends Component {
+
+    state = {
+        zoom: false,
+        size: 'small'
+    };
 
     getImageDetails = async () => {
         const imageDetails = await fetch(this.props.imageUrl);
@@ -31,13 +39,31 @@ class PsBotCardImage extends Component {
 
     };
 
+    zoom = dimmer => () =>
+        this.setState({
+            dimmer,
+            zoom: true,
+    });
+
+    closeZoom = () => {
+        this.setState({
+            zoom: false,
+        });
+    };
+
     render() {
+
+        const { dimmer, zoom, size } = this.state;
+
         return (
+            <div>
             <CardMedia style={{
                 paddingTop: 10,
-                paddingBottom: 10
+                paddingBottom: 10,
+                cursor: 'pointer',
             }}>
                 <img src={this.imageSrc} alt=""
+                     onClick={this.zoom('blurring')}
                     style={{
                         width: this.props.width || '250px',
                         borderRadius: '10px 10px 10px 10px',
@@ -45,6 +71,21 @@ class PsBotCardImage extends Component {
                         marginBottom: '5px',
                     }} />
             </CardMedia>
+
+            <Modal size={size} dimmer={dimmer} open={zoom} style={{
+                    marginTop: 0,
+                    width: '100%',
+                }}
+                closeOnEscape={true}
+                closeOnRootNodeClick={true}
+                onClose={() => this.closeZoom()}
+            >
+                <Modal.Content>
+                    <Image src={this.imageSrc} size="large" />
+                </Modal.Content>
+            </Modal>
+
+            </div>
         );
     }
 }
